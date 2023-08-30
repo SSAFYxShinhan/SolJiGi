00820e96109090fd218209926683119e2f40cdee
heeyeon3050 <kho200a@naver.com>
2023-08-30 22:48:11 +0900

    init

---
+++ src/main/java/com/ssafy/soljigi/SoljigiApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.soljigi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoljigiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoljigiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssafy/soljigi/SoljigiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.soljigi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoljigiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
