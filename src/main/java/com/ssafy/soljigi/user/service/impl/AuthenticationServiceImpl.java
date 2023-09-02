8dc72ba69cf0d52da99051eea24a42b6844dcbf5
rnrudejr9 <rnrudejr1234@naver.com>
2023-09-02 17:27:16 +0900

    feat: 유저 중복 체크 기능 추가

--- src/main/java/com/ssafy/soljigi/user/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/com/ssafy/soljigi/user/service/impl/AuthenticationServiceImpl.java
@@ -1,5 +1,7 @@
 package com.ssafy.soljigi.user.service.impl;
 
+import java.util.Optional;
+
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -10,14 +12,18 @@ import com.ssafy.soljigi.user.dto.request.SigninRequest;
 import com.ssafy.soljigi.user.dto.response.JwtAuthenticationResponse;
 import com.ssafy.soljigi.user.entity.Role;
 import com.ssafy.soljigi.user.entity.User;
+import com.ssafy.soljigi.user.error.AppException;
+import com.ssafy.soljigi.user.error.ErrorCode;
 import com.ssafy.soljigi.user.repository.UserRepository;
 import com.ssafy.soljigi.user.service.AuthenticationService;
 import com.ssafy.soljigi.user.service.JwtService;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 
 @Service
 @RequiredArgsConstructor
+@Log4j2
 public class AuthenticationServiceImpl implements AuthenticationService {
 	private final UserRepository userRepository;
 	private final PasswordEncoder passwordEncoder;
@@ -26,7 +32,12 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
 	@Override
 	public JwtAuthenticationResponse signup(SignUpRequest request) {
-		userRepository.findByUsername(request.getUsername()).ifPresent(a -> new IllegalAccessException("message"));
+		log.info("signup service : " + request);
+		Optional<User> findUser = userRepository.findByUsername(request.getUsername());
+		if (findUser.isPresent()) {
+			throw new AppException(ErrorCode.DUPLICATED_USERNAME);
+		}
+
 		var user = User.builder()
 			.username(request.getUsername())
 			.password(passwordEncoder.encode(request.getPassword()))
@@ -38,6 +49,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
 	@Override
 	public JwtAuthenticationResponse signin(SigninRequest request) {
+		log.info("signin service : " + request);
 		authenticationManager.authenticate(
 			new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
 		var user = userRepository.findByUsername(request.getUsername())
