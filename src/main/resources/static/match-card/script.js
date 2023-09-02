13ea967ea958d8897d8542484d7283a1eb58fea4
h-spear <80024307+h-spear@users.noreply.github.com>
2023-09-02 15:22:04 +0900

    feat: 카드 짝 맞추기 - 랜덤 게임 맵 생성, 정답 처리, 임시 이미지 파일

--- src/main/resources/static/match-card/script.js
+++ src/main/resources/static/match-card/script.js
@@ -1,6 +1,13 @@
 const imgSourcePrefix = './img/friends/';
 const imgSource = [
-    'front.png',
+    'front1.png',
+    'front2.png',
+    'front3.png',
+    'front4.png',
+    'front5.png',
+    'front6.png',
+    'front7.png',
+    'front8.png',
     // 'SOL.png',
     // 'LAY.png',
     // 'LULULALA.png',
@@ -15,47 +22,124 @@ const imgSource = [
     // 'Friends.png',
 ];
 
+let R, C;
 let gameMap = null;
-let cardSelected = null;
+let imgSrcMap = null;
+let cardOpened = null;
+let selectedIdx = 0;
+let matchCardCount = 0;
+let allMatchCount = 0;
+const selectedCursor = [
+    [-1, -1],
+    [-1, -1],
+];
 const gameContent = document.querySelector('.match-card__content');
-// gameContent.appendChild(generateCard(11));
 
-matchCardGameStart(2, 2);
+matchCardGameStart(2, 4);
 
 function matchCardGameStart(row, col) {
+    R = row;
+    C = col;
     gameContent.innerHTML = '';
-    gameMap = Array.from(Array(row), () => new Array(col));
-    cardSelected = Array.from(Array(row), () => new Array(col));
-    constructMap(2, 2);
+    gameMap = Array.from(Array(R), () => new Array(C));
+    imgSrcMap = Array.from(Array(R), () => new Array(C));
+    cardOpened = Array.from(Array(R), () => new Array(C));
+    selectedIdx = 0;
+    matchCardCount = 0;
+    allMatchCount = (row * col) >> 1;
+    constructMap();
+    cardAllOpen();
+    setTimeout(() => cardAllHide(), 1000);
 }
 
-function constructMap(row, col) {
-    for (let i = 0; i < row; ++i) {
+function cardAllOpen() {
+    for (let i = 0; i < R; ++i) {
+        for (let j = 0; j < C; ++j) {
+            cardOpen(i, j);
+        }
+    }
+}
+
+function cardAllHide() {
+    for (let i = 0; i < R; ++i) {
+        for (let j = 0; j < C; ++j) {
+            cardHide(i, j);
+        }
+    }
+}
+
+function cardOpen(x, y) {
+    cardOpened[x][y] = true;
+    gameMap[x][y].classList.add('match-card__card-open');
+}
+
+function cardHide(x, y) {
+    cardOpened[x][y] = false;
+    gameMap[x][y].classList.remove('match-card__card-open');
+}
+
+function checkSelectedPair() {
+    let [fi, fj] = selectedCursor[0];
+    let [si, sj] = selectedCursor[1];
+    if (imgSrcMap[fi][fj] === imgSrcMap[si][sj]) {
+        console.log('match');
+        selectedIdx = 0;
+        ++matchCardCount;
+        if (matchCardCount === allMatchCount) {
+            setTimeout(() => {
+                alert('answer!');
+            }, 1000);
+        }
+    } else {
+        console.log('wrong!!!');
+        setTimeout(() => {
+            cardHide(fi, fj);
+            cardHide(si, sj);
+            selectedIdx = 0;
+        }, 1000);
+    }
+}
+
+function generateRandomCardPairs(count) {
+    let copied = [...imgSource];
+    copied.sort(() => Math.random() - 0.5);
+    copied = copied.slice(0, count).concat(copied.slice(0, count));
+    copied.sort(() => Math.random() - 0.5);
+    console.log(copied);
+    return copied;
+}
+
+function constructMap() {
+    const cardImgs = generateRandomCardPairs(allMatchCount);
+
+    for (let i = 0, idx = 0; i < R; ++i) {
         const rowElement = document.createElement('div');
         rowElement.classList.add('match-card__content-row');
-        for (let j = 0; j < col; ++j) {
-            const flipCardElement = generateCard(0);
+        for (let j = 0; j < C; ++j) {
+            const flipCardElement = generateCardElement(cardImgs[idx]);
             flipCardElement.addEventListener('click', () => {
-                if (!cardSelected[i][j]) {
-                    cardSelected[i][j] = true;
-                    flipCardElement.classList.add('match-card__card-open');
-                } else {
-                    cardSelected[i][j] = false;
-                    flipCardElement.classList.remove('match-card__card-open');
+                if (!cardOpened[i][j] && selectedIdx < 2) {
+                    cardOpen(i, j);
+                    selectedCursor[selectedIdx++] = [i, j];
+                    if (selectedIdx === 2) {
+                        checkSelectedPair();
+                    }
                 }
             });
             rowElement.appendChild(flipCardElement);
             gameMap[i][j] = flipCardElement;
+            imgSrcMap[i][j] = cardImgs[idx];
+            ++idx;
         }
         gameContent.appendChild(rowElement);
     }
 }
 
-function generateCard(index) {
+function generateCardElement(fileName) {
     const flipElement = document.createElement('div');
     const cardElement = document.createElement('div');
     const imgElement = document.createElement('img');
-    imgElement.setAttribute('src', imgSourcePrefix + imgSource[index]);
+    imgElement.setAttribute('src', imgSourcePrefix + fileName);
     imgElement.classList.add('match-card__card-front');
     const backImgElement = document.createElement('img');
     backImgElement.setAttribute('src', imgSourcePrefix + 'back.png');
