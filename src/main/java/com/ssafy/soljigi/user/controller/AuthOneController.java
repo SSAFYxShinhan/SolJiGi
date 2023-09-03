1d418024b3a75e91104ea41579db6a7b45cc1db6
rnrudejr9 <rnrudejr1234@naver.com>
2023-09-03 19:56:34 +0900

    feat: 신한 1원 인증 api 기능 구현

---
+++ src/main/java/com/ssafy/soljigi/user/controller/AuthOneController.java
@@ -0,0 +1,27 @@
+package com.ssafy.soljigi.user.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.soljigi.user.dto.request.AuthOneRequest;
+import com.ssafy.soljigi.user.service.AuthOneService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/auth")
+public class AuthOneController {
+	private final AuthOneService authOneService;
+
+	@PostMapping("/one")
+	public ResponseEntity<?> authOne(@RequestBody AuthOneRequest request) {
+		log.warn(request.getAccountNumber());
+		return ResponseEntity.ok().body(authOneService.authOne(request.getAccountNumber()));
+	}
+}
