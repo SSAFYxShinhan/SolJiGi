6d918ac7c61c9ba2509f13691b34f911e6c52239
JunJI97 <bigwkddo@gmail.com>
2023-09-10 20:32:32 +0900

    feat : 진단검사 - 이미지 맞추기 문제 추가

--- src/main/java/com/ssafy/soljigi/dementiaTest/service/LanguageService.java
+++ src/main/java/com/ssafy/soljigi/dementiaTest/service/LanguageService.java
@@ -15,11 +15,16 @@ public class LanguageService {
 	static final String[] srcImg = new String[] {
 		"https://drive.google.com/file/d/1AQ5GQDW7wP0FjXwAUlQTD3hostHQPGUq/view?usp=drive_link",
 		"https://drive.google.com/file/d/1iZZyZU130J8jdKlCO14wMiEcQ7eotOR_/view?usp=drive_link",
-		"https://drive.google.com/file/d/1yW6iPQbcS05iqsclN-D5o0BFHse87Ib5/view?usp=drive_link"};
-	static final String[] answer = new String[] {"바나나", "시소", "칫솔"};
+		"https://drive.google.com/file/d/1yW6iPQbcS05iqsclN-D5o0BFHse87Ib5/view?usp=drive_link",
+		"https://drive.google.com/file/d/1kFCthi8S1PPoaxfr5jLks7x6h54y2MFs/view?usp=drive_link",
+		"https://drive.google.com/file/d/19cwp-T-uwY_t1unEU7cX7OacDl4Vm5e8/view?usp=drive_link",
+		"https://drive.google.com/file/d/1XepRNRVv2lQdjg3OxbmTW8rIeuVvrDzw/view?usp=drive_link"
+	};
+	static final String[] answer = new String[] {"바나나", "시소", "칫솔","비행기","사과","태권도"};
 
 	public static HashMap<String, Object> getQuiz(HashMap<String, Object> map) {
 		Random random = new Random();
+
 		// Q_img : 이미지로 이름 맞추기
 		ArrayList<String> quiz_img = new ArrayList<>();
 		int qIndex = random.nextInt(srcImg.length);
