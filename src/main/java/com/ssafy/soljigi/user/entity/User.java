05bc9d6742a81b6926a9319211377312596ef826
rnrudejr9 <rnrudejr1234@naver.com>
2023-09-11 23:47:54 +0900

    feat: user 로그인 시 메인화면 변경

--- src/main/java/com/ssafy/soljigi/user/entity/User.java
+++ src/main/java/com/ssafy/soljigi/user/entity/User.java
@@ -18,10 +18,12 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Builder
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "_user")
