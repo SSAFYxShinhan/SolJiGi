65538683235b556b9778890f4181f1e0f40515b3
rnrudejr9 <rnrudejr1234@naver.com>
2023-09-11 22:13:30 +0900

    feat: 회원가입 페이지 수정 + 1원인증 기능 반영

--- src/main/resources/static/view-data/js/sign-up.js
+++ src/main/resources/static/view-data/js/sign-up.js
@@ -1,4 +1,6 @@
 document.getElementById("sign-up-btn").addEventListener("click", joinTest);
+document.getElementById("one-auth-btn").addEventListener("click", oneAuth)
+document.getElementById("one-auth-check-btn").addEventListener("click", oneAuthCheck)
 
 async function joinTest() {
     let response = await fetch("/api/v1/auth/signup", {
@@ -8,7 +10,14 @@ async function joinTest() {
         },
         body: JSON.stringify({
             username: document.getElementById("username").value,
-            password: document.getElementById("password").value
+            password: document.getElementById("password").value,
+            email: document.getElementById("email").value,
+            grade: document.getElementById("grade").value,
+            parentNumber: document.getElementById("parent-number").value,
+            gender: document.getElementById("gender").value,
+            birth: document.getElementById("birth").value,
+            address: document.getElementById("address").value,
+            phoneNumber: document.getElementById("phone-number").value,
         })
     })
     if (response.ok) {
@@ -21,4 +30,50 @@ async function joinTest() {
         console.log(json)
     }
 
+}
+
+
+async function oneAuth() {
+    let response = await fetch("/api/v1/auth/one", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            accountNumber: document.getElementById("account-number").value
+        })
+    })
+    if (response.ok) {
+        console.log(response);
+        let json = await response.json();
+        // location.replace("/view/sign-in");
+        //     Game view 화면
+    } else {
+        console.log(response);
+        let json = await response.text();
+        console.log(json)
+    }
+
+}
+
+async function oneAuthCheck() {
+    let response = await fetch("/api/v1/auth/one", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            accountNumber: document.getElementById("account-number").value
+        })
+    })
+    if (response.ok) {
+        console.log(response);
+        let json = await response.json();
+        // location.replace("/view/sign-in");
+        //     Game view 화면
+    } else {
+        console.log(response);
+        let json = await response.text();
+        console.log(json)
+    }
 }
