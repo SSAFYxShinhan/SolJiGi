d92ed46adeeee2d06ed04df96df445e632c6bfdd
구경덕 <rnrudejr1234@naver.com>
2023-09-11 16:37:01 +0900

    feat: 결과화면 추가 및 컨트롤러 연동

--- src/main/java/com/ssafy/soljigi/user/view/ViewResultController.java
+++ src/main/java/com/ssafy/soljigi/user/view/ViewResultController.java
@@ -7,8 +7,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/view")
 public class ViewResultController {
-	@GetMapping("/result")
+	@GetMapping("/diagnosis/result")
 	public String resultPage() {
-		return "result/result-view";
+		return "result/result-diagnosis-view";
 	}
+
+	@GetMapping("/diagnosis/result/detail/{id}")
+	public String resultDetailPage() {
+		return "result/result-diagnosis-detail-view";
+	}
+
 }
