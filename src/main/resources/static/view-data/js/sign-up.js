8b77d8d07fc8f0e670d8f6f3d041791166b439a6
rnrudejr9 <rnrudejr1234@naver.com>
2023-09-11 22:57:35 +0900

    feat: 1원 인증 기능 구현

--- src/main/resources/static/view-data/js/sign-up.js
+++ src/main/resources/static/view-data/js/sign-up.js
@@ -56,24 +56,11 @@ async function oneAuth() {
 
 }
 
-async function oneAuthCheck() {
-    let response = await fetch("/api/v1/auth/one", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            accountNumber: document.getElementById("account-number").value
-        })
-    })
-    if (response.ok) {
-        console.log(response);
-        let json = await response.json();
-        // location.replace("/view/sign-in");
-        //     Game view 화면
-    } else {
-        console.log(response);
-        let json = await response.text();
-        console.log(json)
+function oneAuthCheck() {
+    let oneAuthKey = document.getElementById("one-auth-check-key").value;
+    if (oneAuthKey === "SSAFY 1234") {
+        document.getElementById("sign-up-btn").className
+            = "btn btn-primary btn-lg";
     }
 }
+
