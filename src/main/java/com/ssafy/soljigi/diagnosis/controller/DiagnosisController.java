375c306cc04f0602fdc8dd0bfeede41e929dfd37
h-spear <80024307+h-spear@users.noreply.github.com>
2023-09-12 20:00:03 +0900

    feat: 진단 검사 저장 API에 SMS 서비스 추가

--- src/main/java/com/ssafy/soljigi/diagnosis/controller/DiagnosisController.java
+++ src/main/java/com/ssafy/soljigi/diagnosis/controller/DiagnosisController.java
@@ -1,5 +1,9 @@
 package com.ssafy.soljigi.diagnosis.controller;
 
+import java.net.URISyntaxException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -12,7 +16,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestClientException;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.ssafy.soljigi.base.api.response.ApiResponse;
 import com.ssafy.soljigi.diagnosis.dto.request.DiagnosisResultSaveRequest;
 import com.ssafy.soljigi.diagnosis.dto.response.DiagnosisResultResponse;
@@ -21,6 +27,8 @@ import com.ssafy.soljigi.diagnosis.service.DiagnosisResultService;
 import com.ssafy.soljigi.diagnosis.service.ExecutiveService;
 import com.ssafy.soljigi.diagnosis.service.MemoryService;
 import com.ssafy.soljigi.diagnosis.service.OrientService;
+import com.ssafy.soljigi.sms.dto.SmsResponseDTO;
+import com.ssafy.soljigi.sms.service.SmsService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -38,6 +46,7 @@ public class DiagnosisController {
 	private final ExecutiveService executiveService;
 	// private final LanguageService languageService;
 	private final DiagnosisResultService resultService;
+	private final SmsService smsService;
 
 	@GetMapping
 	public String diagnosis(Model model) {
@@ -56,9 +65,19 @@ public class DiagnosisController {
 	@PostMapping
 	public ApiResponse<?> saveResult(@RequestBody DiagnosisResultSaveRequest saveRequest) {
 		log.info("saveRequest={}", saveRequest);
-		resultService.save(saveRequest);
+		Map<String, Object> data = new HashMap<>();
+		try {
+			Long savedId = resultService.save(saveRequest);
+			data.put("id", savedId);
+
+			SmsResponseDTO response = smsService.sendDiagnosticResult(saveRequest.getUserId(),
+				saveRequest.getDiagnosticResult());
+			data.put("smsSend", response);
+		} catch (IllegalArgumentException e) {
+			ApiResponse.ofError("존재하지 않는 회원입니다.");
+		}
 		// 보호자에게 sms 문자 전송 기능 추가
-		return ApiResponse.ofSuccess();
+		return ApiResponse.ofSuccess(data);
 	}
 
 	@ResponseBody
@@ -67,4 +86,5 @@ public class DiagnosisController {
 		List<DiagnosisResultResponse> data = resultService.findAll(userId);
 		return ApiResponse.ofSuccess(data);
 	}
+
 }
--- src/main/java/com/ssafy/soljigi/diagnosis/dto/request/DiagnosisResultSaveRequest.java
+++ src/main/java/com/ssafy/soljigi/diagnosis/dto/request/DiagnosisResultSaveRequest.java
@@ -20,4 +20,23 @@ public class DiagnosisResultSaveRequest {
 	private int executiveScore;        // 집행기능
 	private int languageScore;        // 언어
 	private int memoryScore;        // 기억력
+
+	public String getDiagnosticResult() {
+		return "진단 검사 결과가 다음과 같습니다.\n"
+			+ "이름 : 추가예정\n"
+			+ "나이 : " + age + "\n"
+			+ "지남력 : " + orientScore + "/5\n"
+			+ "주의력 : " + attentionScore + "/3\n"
+			+ "시공간기능 : " + spacetimeScore + "/2\n"
+			+ "집행기능 : " + executiveScore + "/6\n"
+			+ "기억력 : " + memoryScore + "/10\n"
+			+ "언어기능 : " + languageScore + "/4\n"
+			+ "총점 : " + getTotalScore() + "\n"
+			+ "검사 진행 결과 : " + "정상";
+	}
+
+	public int getTotalScore() {
+		return orientScore + attentionScore + spacetimeScore
+				+ executiveScore + languageScore + memoryScore;
+	}
 }
