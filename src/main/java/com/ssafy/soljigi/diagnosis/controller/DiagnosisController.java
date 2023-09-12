074f83f202bc580b1155a1532e154f0030d0af5e
JunJI97 <102650903+JunJI97@users.noreply.github.com>
2023-09-13 00:39:15 +0900

    feat : 집행 기능 구현 (#25)
    * refactoring : demenTest 도메인 일괄 삭제(diagnosis와 병합)
    
    * feat : diagnosis 도메인 진단검사 - 언어(Language) 추가
    
    * feat : 진단검사 - 집행(시각추론) 추가
    
    ---------
    
    Co-authored-by: koopi <49141751+rnrudejr9@users.noreply.github.com>

--- src/main/java/com/ssafy/soljigi/diagnosis/controller/DiagnosisController.java
+++ src/main/java/com/ssafy/soljigi/diagnosis/controller/DiagnosisController.java
@@ -25,6 +25,7 @@ import com.ssafy.soljigi.diagnosis.dto.response.DiagnosisResultResponse;
 import com.ssafy.soljigi.diagnosis.service.AttentionService;
 import com.ssafy.soljigi.diagnosis.service.DiagnosisResultService;
 import com.ssafy.soljigi.diagnosis.service.ExecutiveService;
+import com.ssafy.soljigi.diagnosis.service.LanguageService;
 import com.ssafy.soljigi.diagnosis.service.MemoryService;
 import com.ssafy.soljigi.diagnosis.service.OrientService;
 import com.ssafy.soljigi.sms.dto.SmsResponseDTO;
@@ -44,7 +45,7 @@ public class DiagnosisController {
 	private final AttentionService attentionService;
 	// private final SpacetimeService spacetimeService;
 	private final ExecutiveService executiveService;
-	// private final LanguageService languageService;
+	private final LanguageService languageService;
 	private final DiagnosisResultService resultService;
 	private final SmsService smsService;
 
@@ -56,8 +57,9 @@ public class DiagnosisController {
 		model.addAttribute("memory", memoryService.getQuiz(memory4w1h));
 		model.addAttribute("attention", attentionService.getQuiz());
 		model.addAttribute("spacetime", -1);
-		model.addAttribute("executive", executiveService.getQuiz());
-		// model.addAttribute("language", languageService.getQuiz());
+		model.addAttribute("executiveFluency", executiveService.getFluencyQuiz());
+		model.addAttribute("executiveVirtual", executiveService.getVisualQuiz());
+		model.addAttribute("language", languageService.getQuiz());
 		return "diagnosis/diagnosis";
 	}
 
