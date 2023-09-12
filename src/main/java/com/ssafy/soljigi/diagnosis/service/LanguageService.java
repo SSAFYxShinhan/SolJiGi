f30f23f8c457f2db8908b25d080a2e73f51db431
h-spear <80024307+h-spear@users.noreply.github.com>
2023-09-12 23:56:15 +0900

    feat: 진단 검사 결과 저장 API 호출 시 보호자 SMS 알림 서비스 (#24)
    * style: 패키지명 변경(SMS -> sms)
    
    * feat: 진단 검사 저장 API에 SMS 서비스 추가

--- src/main/java/com/ssafy/soljigi/diagnosis/service/LanguageService.java
+++ src/main/java/com/ssafy/soljigi/diagnosis/service/LanguageService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 
-// @Service
+// @service
 @RequiredArgsConstructor
 public class LanguageService {
 
--- src/main/java/com/ssafy/soljigi/sms/dto/MessageDTO.java
+++ src/main/java/com/ssafy/soljigi/sms/dto/MessageDTO.java
@@ -1,10 +1,9 @@
-package com.ssafy.soljigi.SMS.DTO;
+package com.ssafy.soljigi.sms.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @AllArgsConstructor
 @NoArgsConstructor
