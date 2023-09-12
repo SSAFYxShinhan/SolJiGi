63a14c587de88bd13dafee384a5b0904386c49b4
구경덕 <rnrudejr1234@naver.com>
2023-09-12 17:01:34 +0900

    feat: 결과화면 헤더 추가 반영

--- src/main/resources/static/view-data/js/result-diagnosis-info-header.js
+++ src/main/resources/static/view-data/js/result-diagnosis-info-header.js
@@ -1,3 +1,25 @@
 // todayMention 오늘의 한마디
 // weekPayCount 주간 결제건수
 // monthIsDone 월간 검사/ 게임 유무
+
+let weekPayCountArea = document.getElementById("weekPayCount");
+
+async function getTradeInfo() {
+    const url = "";
+    let response = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+
+        })
+    })
+    if (response.ok) {
+        console.log(response);
+        let json = await response.json();
+
+        weekPayCountArea.innerText = "카운트횟수";
+        //     Game view 화면
+    }
+}
