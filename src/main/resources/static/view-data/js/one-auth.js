1c1e5b172b6ce0d402a949c3b84607d425c74347
rnrudejr9 <rnrudejr1234@naver.com>
2023-09-13 01:12:13 +0900

    feat: 1원인증서비스 url수정

--- src/main/resources/static/view-data/js/one-auth.js
+++ src/main/resources/static/view-data/js/one-auth.js
@@ -1,4 +1,25 @@
 document.getElementById("one-auth-btn").addEventListener("click", oneAuth)
+document.getElementById("getTransAction").addEventListener("click", getTransaction);
+document.getElementById("accountConfig").addEventListener("click", oneAuthCheck);
+
+function getTransaction() {
+    Toastify({
+        text: "입금된 KEY는 1 입니다.\n 안녕하세요",
+        duration: 3000,
+        // destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "center", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+            background: "linear-gradient(to right, #00b09b, #96c93d)",
+        },
+        onClick: function () {
+        } // Callback after click
+    }).showToast();
+}
+
 
 async function oneAuth() {
     let response = await fetch("/api/v1/auth/one", {
@@ -13,7 +34,46 @@ async function oneAuth() {
     if (response.ok) {
         console.log(response);
         let json = await response.json();
-        location.replace("/view/sign-in");
+
+        Toastify({
+            text: "입금되었습니다.",
+            duration: 3000,
+            destination: "https://github.com/apvarun/toastify-js",
+            newWindow: true,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: "left", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+                background: "linear-gradient(to right, #00b09b, #96c93d)",
+            },
+            onClick: function () {
+            } // Callback after click
+        }).showToast();
+        //     Game view 화면
+    } else {
+        console.log(response);
+        let json = await response.text();
+        console.log(json)
+    }
+
+}
+
+async function oneAuthCheck() {
+    let url = "/api/v1/auth/one";
+    let response = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            configKey: document.getElementById("configKey").value
+        })
+    })
+    if (response.ok) {
+        console.log(response);
+        let json = await response.json();
+        location.replace("/game");
         //     Game view 화면
     } else {
         console.log(response);
