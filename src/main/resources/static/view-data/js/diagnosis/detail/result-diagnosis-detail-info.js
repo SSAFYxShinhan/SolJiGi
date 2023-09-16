0a0e0c87de91ad203bbeb95b9711153a1ebd7d73
koopi <49141751+rnrudejr9@users.noreply.github.com>
2023-09-16 20:32:27 +0900

    feat: 1원인증 가짜사용자생성, 원형그래프 내용출력, 막대그래프 %표시, 오늘의 한마디 반영 (#54)
    * feat: 상세페이지 원형그래프 출력 데이터 수정, 카카오맵 연동
    
    * feat: 막대그래프 표기법 % 표시, 오늘의 한마디 반영

--- src/main/resources/static/view-data/js/diagnosis/detail/result-diagnosis-detail-info.js
+++ src/main/resources/static/view-data/js/diagnosis/detail/result-diagnosis-detail-info.js
@@ -2,6 +2,32 @@
 Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#858796';
 
+
+var contentString = [
+    "함께 산책하며 시간을 즐기자!",
+    "자연 속에서 신선한 공기 마시며 걷는 건 어때요?",
+    "우리 강아지 친구와 함께 걷기 좋은 날이에요!",
+    "휴식을 즐기면서 걷는 건 어떨까요?",
+    "자연의 아름다움을 느끼며 걸어보세요.",
+    "마음을 정화하고 에너지를 충전하는 산책을 시작해봐요.",
+    "걷면서 새로운 발견을 해보세요!",
+    "산책은 몸과 마음에 좋아요.",
+    "걷기를 시작하면 스트레스가 덜해질 거예요.",
+    "산책 중에 좋은 음악이나 오디오북을 듣는 건 어때요?",
+    "산책을 통해 몸과 마음을 활기차게 만들어봐요.",
+    "우리의 모험을 시작해봅시다!",
+    "이 아름다운 경로에서 함께 걸어요.",
+    "산책은 일상 속에서 작은 여행이에요.",
+    "같이 걷면서 얘기하며 시간을 보내는 건 어때요?",
+    "산책은 건강한 라이프스타일의 일부에요.",
+    "바깥 공기를 마음껏 즐기는 시간이에요.",
+    "새로운 경치를 발견하면서 걷는 것은 흥미로워요.",
+    "산책은 스트레스 해소에 좋아요.",
+    "우리의 건강을 위해 함께 걷는 것이 중요해요."
+]
+
+const randomValue = Math.floor(Math.random() * contentString.length);
+document.getElementById("recommendTextArea").innerText = contentString[randomValue];
 var ctx = document.getElementById("myPieChart");
 
 var resultData1 = document.getElementById("resultData1");
@@ -38,22 +64,22 @@ function renderStickChart() {
     //연산해서 넣기
     let orientWidth = Math.round((orientList[orientList.length - 1] / orient) * 100);
     resultData1.style = `width: ${orientWidth}%`;
-    resultData1Text.innerText = orientWidth;
+    resultData1Text.innerText = orientWidth + "%";
     let attentionWidth = Math.round((attentionList[attentionList.length - 1] / attention) * 100);
     resultData2.style = `width: ${attentionWidth}%`;
-    resultData2Text.innerText = attentionWidth;
+    resultData2Text.innerText = attentionWidth + "%";
     let spacetimeWidth = Math.round((spacetimeList[spacetimeList.length - 1] / spacetime) * 100);
     resultData3.style = `width: ${spacetimeWidth}%`;
-    resultData3Text.innerText = spacetimeWidth;
+    resultData3Text.innerText = spacetimeWidth + "%";
     let executiveWidth = Math.round((executiveList[executiveList.length - 1] / executive) * 100);
     resultData4.style = `width: ${executiveWidth}%`;
-    resultData4Text.innerText = executiveWidth;
+    resultData4Text.innerText = executiveWidth + "%";
     let languageWidth = Math.round((languageList[languageList.length - 1] / language) * 100)
     resultData5.style = `width: ${languageWidth}%`;
-    resultData5Text.innerText = languageWidth;
+    resultData5Text.innerText = languageWidth + "%";
     let memoryWidth = Math.round((memoryList[memoryList.length - 1] / memory) * 100);
     resultData6.style = `width: ${memoryWidth}%`;
-    resultData6Text.innerText = memoryWidth;
+    resultData6Text.innerText = memoryWidth + "%";
 
 
     document.getElementById("resultTotalScore").innerText = datetime +  " / 종합결과"
@@ -134,23 +160,28 @@ async function getMyPayPattern() {
                 cutoutPercentage: 80,
             },
         });
-        document.getElementById("myPieChartLegend").innerHTML =
-            '<div class="mt-4 text-center small">'
-            + '<span class="mr-2">'
-            + '<i class="fas fa-circle text-primary"></i> '
-            + '음식'
-            + '</span> '
-            + '<span class="mr-2"> '
-            + '<i class="fas fa-circle text-success"></i> 커피 </span>'
-            + '<span class="mr-2">'
-            + '<i class="fas fa-circle text-info"></i> 교통 </span>'
-            + '<span class="mr-2">'
-            + '<i class="fas fa-circle text-danger"></i> 옷 </span>'
-            + '<span class="mr-2">'
-            + '<i class="fas fa-circle text-warning"></i> 병원 </span>'
-            + '</div>'
-            + '</div>'
+        var list = Object.keys(transactionData.result);
+        var size = 5;
+        if(list.length > 5){
+            size = 5;
+        }else{
+            size = list.length;
+        }
 
+        var infoDesign = ["text-primary","text-success","text-info","text-danger","text-warning"];
+
+            var str = '<div class="mt-4 text-center small">';
+            for(var step = 0; step < size ;step++){
+                str += '<span class="mr-2">'
+                str += `<i class="fas fa-circle ` + infoDesign[step] + '"></i>'
+                str += list[step];
+                str += '</span> '
+            }
+            str +='</div>'
+            str +='</div>'
+
+
+        document.getElementById("myPieChartLegend").innerHTML = str;
     }
 }
 
