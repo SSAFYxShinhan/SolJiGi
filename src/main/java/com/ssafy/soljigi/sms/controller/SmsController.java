fb7285f021176b338d28f09383b32ed972f3d439
heeyeon3050 <111184269+heeyeon3050@users.noreply.github.com>
2023-09-16 09:30:18 +0900

    fix: 충돌 해결

--- src/main/java/com/ssafy/soljigi/sms/controller/SmsController.java
+++ src/main/java/com/ssafy/soljigi/sms/controller/SmsController.java
@@ -1,42 +1,42 @@
-package com.ssafy.soljigi.sms.controller;
-
-import java.io.UnsupportedEncodingException;
-import java.net.URISyntaxException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.client.RestClientException;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.ssafy.soljigi.sms.dto.MessageDTO;
-import com.ssafy.soljigi.sms.service.SmsService;
-
-import lombok.RequiredArgsConstructor;
-
-@Controller
-@RequiredArgsConstructor
-public class SmsController {
-
-	private final SmsService smsService;
-
-	@GetMapping("/send")
-	public String getSmsPage() {
-		return "SMS/sendSms";
-	}
-
-	@PostMapping("/sms/send")
-	public String sendSms(MessageDTO messageDto, Model model) throws
-		JsonProcessingException,
-		RestClientException,
-		URISyntaxException, InvalidKeyException, NoSuchAlgorithmException, UnsupportedEncodingException {
-		// messageDto.setTo("01088373012");
-		// messageDto.setContent("신한은행 해커톤 sms 테스트");
-		// SmsResponseDTO response = smsService.sendSms(messageDto);
-		// model.addAttribute("response", response);
-		return "SMS/result";
-	}
-}
+//package com.ssafy.soljigi.sms.controller;
+//
+//import java.io.UnsupportedEncodingException;
+//import java.net.URISyntaxException;
+//import java.security.InvalidKeyException;
+//import java.security.NoSuchAlgorithmException;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.client.RestClientException;
+//
+//import com.fasterxml.jackson.core.JsonProcessingException;
+//import com.ssafy.soljigi.sms.dto.MessageDTO;
+//import com.ssafy.soljigi.sms.service.SmsService;
+//
+//import lombok.RequiredArgsConstructor;
+//
+//@Controller
+//@RequiredArgsConstructor
+//public class SmsController {
+//
+//	private final SmsService smsService;
+//
+//	@GetMapping("/send")
+//	public String getSmsPage() {
+//		return "SMS/sendSms";
+//	}
+//
+//	@PostMapping("/sms/send")
+//	public String sendSms(MessageDTO messageDto, Model model) throws
+//		JsonProcessingException,
+//		RestClientException,
+//		URISyntaxException, InvalidKeyException, NoSuchAlgorithmException, UnsupportedEncodingException {
+//		// messageDto.setTo("01088373012");
+//		// messageDto.setContent("신한은행 해커톤 sms 테스트");
+//		// SmsResponseDTO response = smsService.sendSms(messageDto);
+//		// model.addAttribute("response", response);
+//		return "SMS/result";
+//	}
+//}
